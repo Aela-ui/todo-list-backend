ad577a1a5f1c26fe9af7afef645ecc88b302d2dd
Jade Freire de Carvalho <jadefreire4@gmail.com>
2022-08-16 01:22:25 -0300

    add env variables

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,9 +1,11 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = mysql.createPool({
     host: "localhost",
-    user: "jads",
-    password: "jadinha",
+    user: process.env.USER_DATABASE,
+    password: process.env.PASSWORD_DATABASE,
     database: "tudolist"
 });
 
